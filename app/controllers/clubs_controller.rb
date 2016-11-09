58ea6edea5a163af882ba240dce78df435c4b161
sensei100 <jenanton01@gmail.com>
2016-11-09 08:37:35 -0500

    added CRUD actions to clubs_controller

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -6,4 +6,37 @@ class ClubsController < ApplicationController
     render json: clubs
   end
 
+  def show
+    club = Club.find(params[:id])
+    render json: club
+  end
+
+  def create
+    club = Club.new(club_params)
+    if club.save
+      render json: { status: 'ok' }
+    else
+      render json: 
+      { errors: club.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+  def update
+    club = Club.find(params[:id])
+    if club.update(club_params)
+      render json: { status: 'ok' }
+    else
+      render json: 
+      { errors: club.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+  def destroy
+  end
+
+  private
+
+  def club_params
+    params.require(:club).permit(:name, :location)
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit]
 
   def show
-    user = User.find_by_id(params[:id])
+    user = User.find(params[:id])
     render json: user
   end
 
