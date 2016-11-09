ba351bb00881f2501cfc1201d775fbdc7ff03331
sensei100 <jenanton01@gmail.com>
2016-11-09 08:52:58 -0500

    wrote CRUD actions for comments_controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,50 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
+
+  def index
+    comments = Comment.all
+    render json: comments
+  end
+
+  def show
+    comment = Comment.find(params[:id])
+    render json: comment
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: { status: 'ok' }
+    else
+      render json: 
+      { errors: comment.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+    if comment.update(comment_params)
+      render json: { status: 'ok' }
+    else
+      render json: 
+      { errors: comment.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    if comment.user == current_user.id
+      if comment.destroy 
+        render json: { status: 'ok' }
+    else
+      render json: 
+      { errors: comment.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :post_id)
+
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:name, :location)
+    params.require(:post).permit(:content, :user_id, :book_id)
 
 end
