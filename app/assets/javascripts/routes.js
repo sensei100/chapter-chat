81ba57833e4bfe5ec8bb62a2ecf5c5dd7dcf200b
sensei100 <jenanton01@gmail.com>
2016-11-13 09:19:17 -0500

    set up basic states for ui-router

---
+++ app/assets/javascripts/routes.js
@@ -0,0 +1,30 @@
+angular
+  .module('app')
+  .config(function($stateProvider, $urlProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home/home.html',
+        controller: 'HomeController as vm'
+      })
+      .state('home.clubs' {
+        url: '/clubs',
+        templateUrl: 'clubs/clubs.html',
+        controller: 'ClubController as vm'
+      })
+       .state('home.books' {
+        url: '/books',
+        templateUrl: 'books/books.html',
+        controller: 'BookController as vm'
+      })
+        .state('about' {
+        url: '/about',
+        templateUrl: 'about/about.html',
+        controller: 'AboutController as vm'
+      })
+        .state('profile' {
+        url: '/profile',
+        templateUrl: 'profile/profile.html',
+        controller: 'ProfileController as vm'
+      })
+  })
