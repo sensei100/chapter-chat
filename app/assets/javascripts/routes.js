0d7aea6076794bef197533f948392b94325d35a2
sensei100 <jenanton01@gmail.com>
2016-11-13 09:28:06 -0500

    fixed syntax error and added default route

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,35 +1,37 @@
 angular
   .module('app')
-  .config(function($stateProvider, $urlProvider) {
+  .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'home/home.html',
         controller: 'HomeController as vm'
       })
-      .state('home.clubs' {
+      .state('home.clubs', {
         url: '/clubs',
         templateUrl: 'clubs/clubs.html',
         controller: 'ClubController as vm'
       })
-       .state('home.books' {
+       .state('home.books', {
         url: '/books',
         templateUrl: 'books/books.html',
         controller: 'BookController as vm'
       })
-        .state('about' {
+        .state('about', {
         url: '/about',
         templateUrl: 'about/about.html',
         controller: 'AboutController as vm'
       })
-        .state('profile' {
+        .state('profile', {
         url: '/profile',
         templateUrl: 'profile/profile.html',
         controller: 'ProfileController as vm'
       })
-        .state('booksDetails' {
+        .state('booksDetails', {
         url: '/books/details',
         templateUrl: 'views/books/details.html',
         controller: 'BookDetailsController as vm'
-      })
-  })
+      });
+
+        $urlRouterProvider.otherwise('/');
+  });
