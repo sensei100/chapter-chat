c06c8e395a03bc4b61cc4f5298b504a8974b328a
sensei100 <jenanton01@gmail.com>
2016-11-16 07:49:25 -0500

    updated book controller

--- app/assets/javascripts/books/book.controller.js
+++ app/assets/javascripts/books/book.controller.js
@@ -1,8 +1,16 @@
 (function() { 
   'use strict';
 
-  function BookController($scope, $resource) {
-    $scope.name = "test"
+  function BookController($scope, BookService) {
+    $scope.searchTerm = "";
+
+    $scope.doSearch = function() {
+      BookService.get({
+        q: $scope.searchTerm}, function(response) {
+          $scope.bookResults = resonse.items;
+          $scope.orderProp = 'volumeInfo.title';
+        });
+    }
   }
 
   BookController.$inject = ['$scope']
