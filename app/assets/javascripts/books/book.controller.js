95e39082a2e90effbe32cec5df365ba583cac6fa
sensei100 <jenanton01@gmail.com>
2016-11-16 10:07:11 -0500

    able to get api object in console

--- app/assets/javascripts/books/book.controller.js
+++ app/assets/javascripts/books/book.controller.js
@@ -1,10 +1,11 @@
-function BookController($http) {
+function BookController($scope, $http) {
 
       var vm = this;
+      $scope.searchTerm = "jQuery";
       getBooks()
 
       function getBooks() {
-        return $http.get('https://www.googleapis.com/books/v1/volumes', {maxResults: '40', callback: 'JSON_CALLBACK', key: 'AIzaSyCqY1fAVBTbVsXLWfExQKxqQKMgo4vO0SA'})
+        return $http.get('https://www.googleapis.com/books/v1/volumes?q=?&maxResults=40&callback=JSON_CALLBACK&key=AIzaSyCqY1fAVBTbVsXLWfExQKxqQKMgo4vO0SA')
         
           .then(function(response) {
           console.log(response)
@@ -12,7 +13,7 @@ function BookController($http) {
   }
 };
 
-  BookController.$inject = ['$http']
+  BookController.$inject = ['$scope', '$http']
 
 angular
   .module('app')
