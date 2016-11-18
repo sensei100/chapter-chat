443f66c18aa557543def3ab9b961d9be22a42f18
sensei100 <jenanton01@gmail.com>
2016-11-17 21:48:56 -0500

    created nav controller

---
+++ app/assets/javascripts/home/components/nav.controller.js
@@ -0,0 +1,15 @@
+(function() { 
+  'use strict';
+
+  function NavController($scope, Auth) {
+
+
+}
+
+NavController.$inject = ['$scope', Auth]
+
+angular.module('app')
+.controller('NavController' NavController] 
+
+
+}());
