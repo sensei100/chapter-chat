516a1fa55370c1dce402be68209479f27a468995
sensei100 <jenanton01@gmail.com>
2016-11-17 22:04:44 -0500

    updated nav controller

--- app/assets/javascripts/home/components/nav.controller.js
+++ app/assets/javascripts/home/components/nav.controller.js
@@ -2,14 +2,20 @@
   'use strict';
 
   function NavController($scope, Auth) {
+    $scope.signedIn = Auth.isAuthenticated;
+    $scope.logout = Auth.logout;
 
+    Auth.currentUser().then(function (user){
+    $scope.user = user;
+  });
 
 }
 
-NavController.$inject = ['$scope', Auth]
+NavController.$inject = ['$scope', 'Auth']
 
-angular.module('app')
-.controller('NavController' NavController] 
+angular
+  .module('app')
+  .controller('NavController', NavController) 
 
 
 }());
