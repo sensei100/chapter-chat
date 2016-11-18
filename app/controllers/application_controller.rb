f7455d14c831ee9f9131c9cef03b95235ee47be8
sensei100 <jenanton01@gmail.com>
2016-11-17 20:54:30 -0500

    began setting up angular devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   respond_to :json
   before_action :configure_permitted_paramters, if: :devise_controller?
-  
+  after_filter :set_csrf_cookie_for_ng
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 
   def index
   end
 
   protected
+
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
 
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module ChapterChat
   config.assets.paths << Rails.root.join("vendor","assets","bower_components")
   config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
   config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
+  config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
   end
 end
