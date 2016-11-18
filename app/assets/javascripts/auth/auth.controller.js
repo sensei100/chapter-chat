b7f7ab9744f261c93684d151a650ac2400ea00e6
sensei100 <jenanton01@gmail.com>
2016-11-17 22:12:55 -0500

    created auth controller

---
+++ app/assets/javascripts/auth/auth.controller.js
@@ -0,0 +1,24 @@
+(function() { 
+  'use strict';
+
+  function AuthController($scope, $state, Auth) {
+    $scope.login = function() {
+    Auth.login($scope.user).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.register = function() {
+    Auth.register($scope.user).then(function(){
+      $state.go('home');
+    });
+  };
+    
+}
+
+AuthController.$inject = ['$scope', '$state', 'Auth']
+
+angular
+  .module('app')
+  .controller('AuthController', AuthController) 
+}());
--- app/assets/javascripts/home/components/nav.controller.js
+++ app/assets/javascripts/home/components/nav.controller.js
@@ -9,6 +9,18 @@
     $scope.user = user;
   });
 
+    $scope.$on('devise:new-registration', function (e, user){
+    $scope.user = user;
+  });
+
+  $scope.$on('devise:login', function (e, user){
+    $scope.user = user;
+  });
+
+  $scope.$on('devise:logout', function (e, user){
+    $scope.user = {};
+  });
+
 }
 
 NavController.$inject = ['$scope', 'Auth']
